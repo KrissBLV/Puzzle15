82f0d4ac3908f42ffccf9a6311a1c9523680063d
kristofers birks <kristofers.birks@gmail.com>
2020-07-01 17:15:59 +0300

    almost done

--- PuzzleArea.cs
+++ PuzzleArea.cs
@@ -16,22 +16,80 @@ namespace Puzzle15
         {
             InitializeComponent();
             InitializePuzzleArea();
-            InitializeButtons();
+            InitializeBLocks();
         }
 
         private void InitializePuzzleArea()
         {
             this.BackColor = Color.LightGreen;
             this.Text = "Puzzle 15";
+            this.ClientSize = new Size(800, 800);
         }
 
-        private void InitializeButtons()
+        private void InitializeBLocks()
         {
-            Button button;
-            for(int i = 1; i < 16; i++ )
+            int blockCount = 1;
+            PuzzleBlock block;
+            for(int row = 1; row < 5; row++ )
             {
-                button = new Button();
-                this.Controls.Add(button);
+                for(int col = 1; col < 5; col++)
+                {
+                    block = new PuzzleBlock();
+                    block.Top = row * 155;
+                    block.Left = col * 155;
+                    block.Text = blockCount.ToString();
+                    Name = "Block" + blockCount.ToString();
+                    
+
+                    //block.Click += new EventHandler(Block_Click);
+                    block.Click += Block_Click;
+                    if(blockCount == 16)
+                    {
+                        block.Name = "EmptyBlock";
+                        block.Text = "";
+                        block.BackColor = Color.AntiqueWhite;
+                    }
+                    blockCount++;
+                    this.Controls.Add(block);
+                }
+            }
+        }
+
+        private void Block_Click(object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            if (IsAdjacent(block))
+            {
+                SwapBlocks(block);
+            }
+        }
+
+        private void SwapBlocks(Button block)
+        {
+            Button zeroBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = zeroBlock.Location;
+            zeroBlock.Location = oldLocation;
+            
+        }
+
+        private bool IsAdjacent(Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button zeroBlock = (Button)this.Controls["EmptyBlock"];
+
+            a = Math.Abs(zeroBlock.Top - block.Top);
+            b = Math.Abs(zeroBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if(c < 156)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
---
+++ PuzzleBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle15
+{
+    class PuzzleBlock : Button
+    {
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock();
+        }
+
+        private void InitializePuzzleBlock()
+        {
+            this.BackColor = Color.LightYellow;
+            this.Height = 150;
+            this.Width = 150;
+            this.Text = "0";
+            this.Font = new Font("Roboto", 18);
+        }
+    }
+}
